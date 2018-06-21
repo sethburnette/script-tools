fd0bc12cf1491c05128bfbcb12fe2bd2a959ffc8
sethburnette <seth.burnette@gmail.com>
2018-06-21 18:43:15 -0400

    Added display for keyboard example
    With a simple script on each key, the keyboard trigger events now kick off OnClick actions which show the name of the key being pressed/triggered on a display above the keyboard.

---
+++ ScriptTools/Assets/Toolbox/TriggerToClick/Scripts/KeyPress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyPress : MonoBehaviour {
+
+    public GameObject display;
+    private Text displayText;
+    private string buttonName;
+
+    void Start()
+    {
+        buttonName = gameObject.name;
+        if (display.GetComponent<Text>() != null)
+        {
+            displayText = display.GetComponent<Text>();
+        }
+    }
+
+	public void SayMyName ()
+    {
+        if (display != null)
+        {
+            displayText.text = buttonName;
+        }        
+	}
+}
