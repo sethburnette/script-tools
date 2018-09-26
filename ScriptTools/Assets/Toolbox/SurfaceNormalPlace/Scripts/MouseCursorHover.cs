116fc2cd5cd41db189d81ddb046d8a97f4512df1
sethburnette <seth.burnette@gmail.com>
2018-09-26 10:21:55 -0400

    Added a small mouse cursor script
    This script is useful for setting up alternate mouse cursors for interaction indicators.

---
+++ ScriptTools/Assets/Toolbox/SurfaceNormalPlace/Scripts/MouseCursorHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorHover : MonoBehaviour {
+
+    public Texture2D customCursor;
+    public CursorMode curMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+	void Start ()
+    {
+        Cursor.SetCursor(null, hotSpot, curMode);
+    }
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(customCursor, hotSpot, curMode);
+    }
+
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(null, hotSpot, curMode);
+    }
+}
